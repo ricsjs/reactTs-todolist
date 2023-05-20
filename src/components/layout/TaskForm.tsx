5f9d35d550143bd1b3b573cc2189d8d8367af2ed
Ricardo Alencar <ricfilho0007@gmail.com>
2023-05-20 15:20:51 -0300

    form styles

--- src/components/layout/TaskForm.tsx
+++ src/components/layout/TaskForm.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import './TaskForm.css'
 
 export interface Props {
     btnText: string
@@ -7,13 +8,13 @@ export interface Props {
 export default class TaskForm extends React.Component<Props> {
     public render() {
         return (
-            <form>
-                <div>
+            <form className='form'>
+                <div className='input-container'>
                     <label htmlFor='title'>Título:</label>
                     <input name='title' type='text' placeholder='Título da tarefa' />
                 </div>
 
-                <div>
+                <div className='input-container'>
                     <label htmlFor='difficulty'>Dificuldade:</label>
                     <input name='difficulty' type='text' placeholder='Dificuldade da tarefa' />
                 </div>
